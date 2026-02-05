f0cbad4b90ad4360f8e6211bbe8caad4139c49c5
Jessica <jessica.antoinette@outlook.com>
2026-02-05 09:03:17 +0000

    initial commit

---
+++ client/src/main.js
@@ -0,0 +1,56 @@
+const display = document.getElementById('app');
+const form = document.getElementById('form');
+const baseURL = 'http://localhost:7777/ParanormalExperiences'
+
+
+async function fetchData() {
+  const response = await fetch(`http://localhost:7777/ParanormalExperiences`);
+  const data = await response.json();
+
+  return data;
+};
+
+
+async function displayMessages() {
+  display.innerHTML = "";
+  const ParanormalExperiences = await fetchData();
+
+  ParanormalExperiences.forEach((message) => {
+    const row = document.createElement('div');
+    row.classList.add('row');
+
+    row.innerHTML = `
+      <span>${message.name}</span>
+      <span>${new Date(message.date).toLocaleDateString()}</span>
+      <span>${message.event}</span>
+    `;
+
+    display.appendChild(row);
+  });
+}
+
+
+
+async function handleSubmit(e) {
+  e.preventDefault();
+
+  const formData = new FormData(form);
+  const userInput = Object.fromEntries(formData.entries());
+
+  const response = await fetch(baseURL, {
+    headers: { "Content-Type": "application/json"},
+    method:"POST",
+    body: JSON.stringify(userInput),
+  });
+
+if (!response.ok) {
+  const error = await response.json();
+  console.error("Ghosts ate your entry:", error);
+}
+
+form.reset();
+await displayMessages();
+}
+
+form.addEventListener('submit', handleSubmit);
+displayMessages();
---
+++ server/server.js
@@ -0,0 +1,54 @@
+import express from "express"
+import pg from "pg"
+import cors from "cors"
+import dotenv from "dotenv"
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+dotenv.config();
+
+const db = new pg.Pool({
+    connectionString: process.env.DB_CONN,
+});
+
+app.get('/', (request, response) => {
+    response.status(200).json({ status: "ok"});
+});
+
+
+app.get('/ParanormalExperiences', async (request, response) => {
+try {
+    const data = await db.query(`SELECT * FROM "ParanormalExperiences"`);
+
+    response.status(200).json(data.rows);
+}
+catch (err) {
+    console.error(err);
+    response.status(500).json({error: err.message})
+}
+});
+
+app.post('/ParanormalExperiences', async (request, response) => {
+    try {
+    const { name, date, event } = request.body
+
+    const dbQuery = await db.query(`INSERT INTO "ParanormalExperiences" (name, date, event) VALUES ($1, $2, $3)`, [name, date, event]);
+
+    response.status(200).json({ success: true});
+    response.status(201).json(dbQuery.rows[0]);
+
+}   catch (err) {
+        console.error(err);
+        response.status(500).json({ error: err.message});
+    }
+});
+
+
+
+
+app.listen(7777, () => {
+    console.log("server listening for requests made to http://localhost:7777");
+});
+
